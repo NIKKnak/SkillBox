b1b6d17245e76f6bbe8223a2e9d920eb70596ce8
NIKKnak <nik88350759@yandex.ru>
2024-04-05 15:50:56 +0300

    MethodsDZ2+

---
+++ Methods/DZ2/ReversString.cs
@@ -0,0 +1,38 @@
+﻿using Methods.DZ1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods.DZ2
+{
+    internal class ReversString
+    {
+        public void ReversStringText(string inputString)
+        {
+
+            SplitText splitText = new SplitText();
+            string[] brokenString = splitText.SplitInputText(inputString);
+
+            int a = brokenString.Length;
+
+            StringBuilder sb = new StringBuilder(50);
+
+            while (a >0)
+            {
+                sb.Append(brokenString[a-1] + " ");
+                a--;
+            }
+
+            Console.WriteLine(sb);
+
+        }
+
+
+
+
+
+
+    }
+}
--- Methods/Program.cs
+++ Methods/Program.cs
@@ -1,19 +1,29 @@
-﻿namespace Methods
+﻿using Methods.DZ1;
+using Methods.DZ2;
+
+
+
+namespace Methods
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"Введите слова через пробел\n\n");
+            /*Console.WriteLine($"Введите слова через пробел\n\n");
             string inputText = Console.ReadLine();
 
             SplitText splitText = new SplitText();
             PrintConvertString printConvertString = new PrintConvertString();
 
-            printConvertString.PrintArray(splitText.SplitInputText(inputText));
+            printConvertString.PrintArray(splitText.SplitInputText(inputText));*/
 
+            //-----------------------------------------------------------------------
 
-           
+            Console.WriteLine($"Введите слова через пробел\n\n");
+            string inputText = Console.ReadLine();
+
+           ReversString reversString = new ReversString();
+            reversString.ReversStringText(inputText);
 
 
 
