97b4226fc2bd7b628349bf1f7a9a02a4fb1211dc
Николай Ветошкин <nik88350759@yandex.ru>
2024-04-09 16:25:34 +0300

    Employees DZ Start

---
+++ Employees/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Employees
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            a();
+            
+
+            
+
+
+        }
+
+        public static void a()
+        {
+            var data = DateTime.Now;
+
+            int sum = 0;
+
+            for (int i = 0; i < 1000000000; i++)
+            {
+                sum += i;
+
+            }
+            Console.WriteLine(sum);
+            TimeSpan timeSpan = DateTime.Now - data;
+            Console.WriteLine(timeSpan.Milliseconds);
+        }
+
+
+    }
+}
