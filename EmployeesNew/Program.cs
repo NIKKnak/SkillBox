d22dea93bf618be1afcf0d2daafd087ce0225a35
NIKKnak <nik88350759@yandex.ru>
2024-04-11 17:35:26 +0300

    EmployeesNew Start

---
+++ EmployeesNew/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeesNew
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
