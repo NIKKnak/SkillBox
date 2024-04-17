ee6b52e0fbd001e7195ed0516f77286f8efbf31e
NIKKnak <nik88350759@yandex.ru>
2024-04-17 17:26:02 +0300

    FoneBook 70%

---
+++ Lists/FoneBook.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Lists
+{
+    internal class FoneBook
+    {
+
+        public Dictionary<int, string> addContakt(int number, string fio, Dictionary<int, string> myDictionary)
+        {
+            if (number == null || fio == null)
+            {
+                return myDictionary;
+            }
+            else
+            {
+                myDictionary.Add(number, fio);
+                return myDictionary;
+            }
+        }
+
+        public void PrintValue(Dictionary<int, string> myDictionary)
+        {
+            Console.WriteLine("Введите номер контакта");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            string value = myDictionary[number];
+            
+            Console.WriteLine();
+            Console.WriteLine($"Телефон: {number} Пользователь: {value}");
+        }
+
+
+        public void PrintDictionary(Dictionary<int, string> myDictionary)
+        {
+            foreach (KeyValuePair<int, string> item in myDictionary)
+            {
+                Console.WriteLine($"Телефон: {item.Key} Пользователь: {item.Value}");
+            }
+        }
+
+
+
+
+
+    }
+}
--- Lists/Program.cs
+++ Lists/Program.cs
@@ -5,8 +5,8 @@
         static void Main(string[] args)
         {
             #region DZ1
-            WorkWithList workWithList = new WorkWithList();
-            
+            /*WorkWithList workWithList = new WorkWithList();
+
             List<int> myList = new List<int>();
 
             workWithList.FillList(myList);
@@ -14,9 +14,57 @@
             workWithList.DelValue(myList);
             Console.WriteLine();
             Console.WriteLine();
-            workWithList.PrintList(myList);
+            workWithList.PrintList(myList);*/
             #endregion DZ1
 
+            #region FoneBook
+
+            Dictionary<int, string> myDictionary = new Dictionary<int, string>();
+            FoneBook foneBook = new FoneBook();
+
+
+            while (true)
+            {
+
+
+
+                Console.WriteLine($"Добавить контакт: 1\nНайти контакт: 2\nПоказать все контакты: 3\n");
+                int inputValue = Convert.ToInt32(Console.ReadLine());
+                switch (inputValue)
+                {
+                    case 1:
+
+
+
+                        Console.WriteLine($"Введите номер телефона пользователя");
+                        int foneNumber = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine($"Введите ФИО пользователя");
+                        string fio = Console.ReadLine();
+
+                        foneBook.addContakt(foneNumber, fio, myDictionary);
+                        Console.Clear();
+                        break;
+
+                    case 2:
+                        foneBook.PrintValue(myDictionary);
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+
+                    case 3:
+                        foneBook.PrintDictionary(myDictionary);
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+
+
+                }
+
+
+            }
+
+            #endregion FoneBook
 
         }
     }
