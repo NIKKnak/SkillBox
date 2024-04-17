1a7bab3d27d3836b0de9d0f30484c9d51b4aeb39
NIKKnak <nik88350759@yandex.ru>
2024-04-17 15:28:43 +0300

    WorkWithList dz1+

---
+++ Lists/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region DZ1
+            WorkWithList workWithList = new WorkWithList();
+            
+            List<int> myList = new List<int>();
+
+            workWithList.FillList(myList);
+            workWithList.PrintList(myList);
+            workWithList.DelValue(myList);
+            Console.WriteLine();
+            Console.WriteLine();
+            workWithList.PrintList(myList);
+            #endregion DZ1
+
+
+        }
+    }
+}
---
+++ Lists/WorkWithList.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists
+{
+    internal class WorkWithList
+    {
+
+        public List<int> FillList(List<int> myList)
+        {
+
+            Random random = new Random();
+
+            Console.WriteLine("Заполненный лист");
+            for (int i = 0; i <= 100; i++)
+            {
+                myList.Add(random.Next(0, 100));
+            }
+
+            return myList;
+        }
+
+        public void PrintList(List<int> myList)
+        {
+            myList.Sort();
+
+            foreach (var item in myList)
+            {
+                Console.Write(item + " ");
+            }
+
+        }
+
+        public List<int> DelValue(List<int> myList)
+        {
+            myList.RemoveAll(x => x > 25 && x < 50); // не работает без sort           
+            return myList;
+        }
+
+    }
+}
