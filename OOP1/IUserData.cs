4c0a007b9157fc95dae5d1eda4aeec4dc0d1f832
NIKKnak <nik88350759@yandex.ru>
2024-04-25 17:32:49 +0300

    OOP1

---
+++ OOP1/IUserData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    interface IUserData
+    {
+        string Name { get; set; }
+        string Surname {  get; set; }
+        string Patronymic { get; set; }
+        decimal PhoneNumber { get; set; }
+        int Series { get; set; }
+        int PassportID { get; set; }
+
+        void ViewingInformation() { }
+    }
+}
+
+/*Фамилия
+Имя
+Отчество
+Номер телефона
+Серия, номер паспорта*/
---
+++ OOP1/UserBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    class UserBase : IUserData
+    {
+        public string Name { get ; set; }
+        public string Surname { get; set; }
+        public string Patronymic { get; set; }
+        public decimal PhoneNumber { get ; set; }
+        public int Series { get; set; }
+        public int PassportID { get ; set; }
+
+        public UserBase(string name, string surname, string patronymic, decimal phoneNumber, int series, int passportId)
+        { 
+            this.Name = name;
+            this.Surname = surname;
+            this.Patronymic = patronymic;
+            this.PhoneNumber = phoneNumber;
+            this.Series = series;
+            this.PassportID = passportId;    
+        }
+    }
+}
