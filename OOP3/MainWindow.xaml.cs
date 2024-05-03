58707ec23a34dd72b956d9567f8ea76c5a677276
NIKKnak <nik88350759@yandex.ru>
2024-05-03 16:28:03 +0300

    OOP3 start

---
+++ OOP3/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OOP3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Client<int>> clients = new List<Client<int>>();
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            WindowAddClient windowAddClient = new WindowAddClient();
+            windowAddClient.ShowDialog();
+        }
+    }
+}
