8ac0dde70ea26b7d0b5ed6e92d8adb1645f7c89c
NIKKnak <nik88350759@yandex.ru>
2024-05-08 16:41:33 +0300

    test +

---
+++ Test/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Test
+{
+    internal class Program
+    {
+        public static int o = 0;
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+            Class2 class2 = new Class2();
+
+            
+
+            Thread thread1 = new Thread(class1.Method1);
+
+            Thread thread2 = new Thread(class2.Method2);
+
+            thread1.Start();
+            thread2.Start();
+
+        }
+
+
+    }
+}
