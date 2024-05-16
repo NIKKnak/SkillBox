a2238dba9db671b615ab5666ccefae144b68c402
NIKKnak <nik88350759@yandex.ru>
2024-05-16 17:44:30 +0300

    SqlConnectionStringBuilder test

---
+++ ADOnetDZ/Program.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+
+
+namespace ADOnetDZ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //string s = @"Data Source=(LocalDB)\MSSQLLocalDB;
+            //AttachDbFilename=C:\Users\nik88\OneDrive\Документы\MSSQLLocalDb.mdf;
+            //Integrated Security=True;
+            //Connect Timeout=30;
+            //Encrypt=True";
+
+            SqlConnectionStringBuilder strCon = new SqlConnectionStringBuilder()
+            {
+                DataSource = @"(LocalDB)\MSSQLLocalDB",
+                InitialCatalog = "C:\\Users\\nik88\\OneDrive\\Документы\\MSSQLLocalDb.mdf",
+                IntegratedSecurity = true,
+                Pooling = true,
+
+            };
+
+
+
+            // чтение данных из базы
+            try
+            {
+                using (SqlConnection c = new SqlConnection(strCon.ConnectionString))
+                {
+                    c.Open();
+                    //Console.WriteLine(c.State);
+                    var sql = @"select 
+                                Id as 'id',
+                                WorkerName as 'name',
+                                departmantName as 'department'
+                                from Bosses";
+
+                    SqlCommand sqlCommand = new SqlCommand(sql, c);
+                    SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+                    while (sqlDataReader.Read())
+                    {
+                        Console.WriteLine($"{sqlDataReader[0],4} | " +
+                                          $"{sqlDataReader[1],4} | " +
+                                          $"{sqlDataReader["department"],4} | ");
+
+                    }
+
+
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+
+
+
+        }
+    }
+}
--- Test/Program.cs
+++ Test/Program.cs
@@ -5,39 +5,17 @@ namespace Test
 {
     internal class Program
     {
+
+        static Random rnd = new Random();
+
+
         static void Main(string[] args)
         {
-            Task task1 = Task.Run(() => MyClass1("Сообщение 1", 2000));
-            Task task2 = Task.Run(() => MyClass2("Сообщение 2", 3000));
-            Task task3 = Task.Run(() => MyClass3("Сообщение 3", 1500));
 
-            Task.WaitAll(task1, task2, task3);
-        }
 
-        static async Task MyClass1(string message, int delay)
-        {
-            Thread.CurrentThread.Name = "Поток 1";
 
-            await Task.Delay(delay);
 
-            Console.WriteLine($"{message} - Поток: {Thread.CurrentThread.Name}");
-        }
 
-        static async Task MyClass2(string message, int delay)
-        {
-            Thread.CurrentThread.Name = "Поток 2";
-
-            await Task.Delay(delay);
-
-            Console.WriteLine($"{message} - Поток: {Thread.CurrentThread.Name}");
-        }
-        static async Task MyClass3(string message, int delay)
-        {
-            Thread.CurrentThread.Name = "Поток 3";
-
-            await Task.Delay(delay);
-
-            Console.WriteLine($"{message} - Поток: {Thread.CurrentThread.Name}");
         }
 
 
